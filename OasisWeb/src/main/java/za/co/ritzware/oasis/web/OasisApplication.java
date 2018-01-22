a391a94e51513943f8871cc042528311e93b7f33
James Ritala <jritala@jhb.dvt.co.za>
2018-01-22 21:19:09 +0200

    Removed unused imports

--- OasisWeb/src/main/java/za/co/ritzware/oasis/web/OasisApplication.java
+++ OasisWeb/src/main/java/za/co/ritzware/oasis/web/OasisApplication.java
@@ -1,12 +1,8 @@
 package za.co.ritzware.oasis.web;
 
-import java.util.Date;
-
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
