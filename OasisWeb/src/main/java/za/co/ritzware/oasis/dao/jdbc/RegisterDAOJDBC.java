320bc5ebba6005e59f15069fd33b16a55cf0b209
James Ritala <jritala@jhb.dvt.co.za>
2018-01-29 21:38:11 +0200

    Modified JDBC DAO code to add implementation

--- OasisWeb/src/main/java/za/co/ritzware/oasis/dao/jdbc/RegisterDAOJDBC.java
+++ OasisWeb/src/main/java/za/co/ritzware/oasis/dao/jdbc/RegisterDAOJDBC.java
@@ -1,19 +1,32 @@
 package za.co.ritzware.oasis.dao.jdbc;
 
+import java.sql.ResultSet;
 import java.util.List;
  
 import za.co.ritzware.oasis.dao.RegisterDAO;
 import za.co.ritzware.oasis.model.RegistrationBean;
+import za.co.ritzware.oasis.utils.DatabaseUtilities;
 
 public class RegisterDAOJDBC implements RegisterDAO {
-	private static String SAVE_QUERY="insert into Application(";
+	private static String SAVE_QUERY="INSERT INTO application(firstname,lastname,natid,gender,dob,maritalstatus,cellnumber,email,occupation,companyname,income,empperiod,workphone,incometaxnumber,methodofcontact,typeofplace,lengthofstay)";
 	/* (non-Javadoc)
 	 * @see za.co.ritzware.oasis.dao.jdbc.RegisterDAO#save(za.co.ritzware.oasis.model.RegistrationBean)
 	 */
 	@Override
 	public String save(RegistrationBean application) {
 		String refNum="";
+		SAVE_QUERY = SAVE_QUERY + " VALUES (";
 		
+		SAVE_QUERY = SAVE_QUERY + "'"+ application.getFirstName()+"','"+ application.getLastName()+"','"
+				+ application.getId()+"','"+ application.getGender()+"','"+ application.getDateOfBirth()+"','"
+				+application.getMaritalStatus()+"','"+ application.getCellNumber()+"','"+application.getEmail()+"','"
+				+application.getOccupation()+"','"+application.getCompanyName()+"',"+application.getMonthlyIncome()+",'"
+				+ application.getEmploymentPeriod()+"','"+ application.getWorkPhone()+"','"+application.getIncomeTaxNumber()+"','"
+				+ application.getMethodOfContact()+"','"+ application.getTypeOfPlace()+"','"+application.getLengthOfStay()+"')";
+		ResultSet rs= DatabaseUtilities.executeQuery(SAVE_QUERY);
+	     if(rs!=null) {
+	    	  //TODO
+	     }
 		 return refNum;
 	}
 	
